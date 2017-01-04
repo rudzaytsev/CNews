d9887bd02d717a7c1418f1448f049fb856307c40
rudzaytsev <rudzaytsev@gmail.com>
2017-01-04 19:51:48 +0300

    #2 Added spring-boot-starter-freemarker. Added trivial FreeMarker template for main page.

--- src/main/java/cnews/SampleController.java
+++ src/main/java/cnews/SampleController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.*;
 public class SampleController {
 
   @RequestMapping("/")
-  @ResponseBody
-  String home() {
-    return "Happy New Year 2017!";
+  public String home() {
+    return "main";
   }
+
+
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(SampleController.class, args);
